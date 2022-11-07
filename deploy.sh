16557055951020b11e5a0e84471098b7a4660b79
Andrii Bondarenko <andr780008@gmail.com>
2022-11-07 21:02:09 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push andr095/multi-server:$SHA
 docker push andr095/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployment/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployment/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployment/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployment/client-deployment client=andr095/multi-client:$SHA
+kubectl set image deployment/server-deployment server=andr095/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=andr095/multi-worker:$SHA
