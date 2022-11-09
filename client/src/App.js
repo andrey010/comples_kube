83a95c9ee39f26020923afb8dd70827a6b69ffac
Andrii Bondarenko <andr780008@gmail.com>
2022-11-09 12:30:16 +0200

    add skaffold config

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h2>Calculator fibonacci</h2>
+        <h2>Calculator fibonacci !!!!</h2>
         <div>
           <Route exact path="/" component={Fib} />
         </div>
